9c7dcaaeaea430fbf5bd35a69bec9b760ab4a57e
649433919@qq.com <649433919@qq.com>
2022-05-10 12:07:54 +0000

    cast

---
+++ cast/build/CMakeFiles/Cast.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cast.dir/cast.cpp.o"
+  "CMakeFiles/Cast.dir/cast.cpp.o.d"
+  "Cast"
+  "Cast.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
