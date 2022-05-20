ab4572d74f0fb6b8e907801bf7c66ebb2ea8610b
fuyou765 <649433919@qq.com>
2022-05-20 02:29:33 +0000

    threadpool

---
+++ design_patern/threadpool/build/CMakeFiles/thread_pool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread_pool.dir/main.cc.o"
+  "CMakeFiles/thread_pool.dir/main.cc.o.d"
+  "thread_pool"
+  "thread_pool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thread_pool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
