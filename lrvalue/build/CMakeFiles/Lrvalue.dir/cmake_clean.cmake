2f580ff065ca063be15b45e79afd75ec3e260b8e
fuyou765 <649433919@qq.com>
2022-05-25 21:04:55 -0900

    lrvalue

---
+++ lrvalue/build/CMakeFiles/Lrvalue.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lrvalue.dir/lrvalue.cpp.o"
+  "CMakeFiles/Lrvalue.dir/lrvalue.cpp.o.d"
+  "Lrvalue"
+  "Lrvalue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lrvalue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lrvalue/build/CMakeFiles/Lrvalue.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lrvalue.
+# This may be replaced when dependencies are built.
